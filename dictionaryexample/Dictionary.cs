25299cd92ce701cb9886538cf81bce1a97e3b92e
İsa Talha MENGİ <sa Talha MENG@DESKTOP-3FTB8BO>
2022-06-29 20:19:24 +0300

    Add project files.

---
+++ dictionaryexample/Dictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionaryexample
+{
+    public class Dictionaryy<L,R>
+    {
+        L[] Leftvalue;
+        R[] Rightvalue;
+        public Dictionaryy()
+        {
+            Leftvalue = new L[0];
+            Rightvalue = new R[0];
+        }
+
+        public void Add(L solitem, R sagitem)
+        {
+            L[] temparraysol = Leftvalue;
+            R[] temparraysag = Rightvalue;
+
+            Leftvalue = new L[Leftvalue.Length + 1];
+            Rightvalue = new R[Rightvalue.Length + 1];
+            for (int i = 0; i < temparraysol.Length; i++)
+            {
+                Leftvalue[i] = temparraysol[i];
+                Rightvalue[i] = temparraysag[i];
+            }
+            Leftvalue[Leftvalue.Length - 1] = solitem;
+            Rightvalue[Rightvalue.Length - 1] = sagitem;
+            
+        }
+
+       
+        public L [] soldeger
+        {
+            get { return Leftvalue; }
+        }
+        public R [] sagdeger
+        {
+            get { return Rightvalue; }
+        }
+    }
+}
---
+++ dictionaryexample/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using dictionaryexample;
+
+Console.WriteLine("Hello, World!");
+
+Dictionaryy<string,string> dictionaryy = new Dictionaryy<string,string>();
+
+dictionaryy.Add("Emir", "İsa");
+
+foreach (var item in dictionaryy.soldeger)
+{
+    Console.WriteLine(item);
+}
+foreach (var item in dictionaryy.sagdeger)
+{
+    Console.WriteLine(item);
+}
+
+
